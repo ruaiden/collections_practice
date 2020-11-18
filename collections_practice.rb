178e1a64a55fc0e1048ae46a8ead6e606a00e33a
Aiden <aiden.marques94@gmail.com>
2020-11-18 09:16:27 -0500

    commit tree d073f5a072461b7e292d90b94325affe3ee1a5b3

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,45 @@
+def sort_array_asc(numbers)
+    numbers.sort 
+end 
 
+
+def sort_array_desc(numbers)
+    numbers.sort{ |x,y| y <=> x} 
+end
+
+
+def sort_array_char_count(numbers)
+    numbers.sort{|left,right| left.length <=> right.length}
+end
+
+def swap_elements(letters)
+    letters[0], letters[1], letters[2] = letters[0], letters[2], letters[1]
+end 
+
+
+def reverse_array(order)
+    order.reverse
+end 
+
+def kesha_maker(money)
+    money.each {|dollar| dollar[2] = "$" } 
+end 
+
+def find_a(words)
+    words.select {|word| word.start_with?("a")}
+end 
+
+def sum_array(sum)
+    sum.inject { |number, n| number + n}
+end
+
+def add_s(word)
+
+    word.map do |x|
+        if x != word[1] 
+            x << "s"
+        else 
+            x
+        end 
+    end 
+end 
